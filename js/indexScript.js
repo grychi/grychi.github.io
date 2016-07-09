36e1a449da193435061c3fd2089d9e8946f7a44d
gbeargary <gc1753@nyu.edu>
2016-07-09 16:51:18 -0400

    started mobile menu

--- js/indexScript.js
+++ js/indexScript.js
@@ -4,16 +4,31 @@ $(document).ready(function() {
 		hideAllRes();
 		$("#skillsCont").css("display","block");
 	});
-	
 	$("#tools").click(function() {
 		hideAllRes();
 		$("#toolsCont").css("display","block");
 	});
-	
 	$("#exp").click(function() {
 		hideAllRes();
 		$("#expCont").css("display","block");
 	});
+	
+	$("#burger").click(function() {
+		$(this).toggle();
+		$("#close").toggle();
+		$("#mMenu").toggle();
+	});
+	$("#close").click(function() {
+		$(this).toggle();
+		$("#burger").toggle();
+		$("#mMenu").toggle();
+	});
+	$("#mMenu").click(function() {
+		$(this).toggle();
+		$("#close").toggle();
+		$("#burger").toggle();
+	});
+	
 });
 
 function hideAllRes() {
