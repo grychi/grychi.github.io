7a31c413ac39b8d85323b7c228db91c7c5542fd0
gbeargary <gc1753@nyu.edu>
2017-02-19 21:10:15 -0500

    testing

---
+++ rebecky/js/index.js
@@ -0,0 +1,58 @@
+var lastScrollTop = 0;
+
+$('document').ready(function () {
+    
+    $("#navMain").hover(function () {
+        $("#theDes").html("Overview");
+    },
+        function () {
+            resetDes();
+        });
+    $("#navEdu").hover(function () {
+        $("#theDes").html("Education");
+    },
+        function () {
+            resetDes();
+        });
+    $("#navWork").hover(function () {
+        $("#theDes").html("Work Experience");
+    },
+        function () {
+            resetDes();
+        });
+    $("#navIntHob").hover(function () {
+        $("#theDes").html("Interests & Hobbies");
+    },
+        function () {
+            resetDes();
+        });
+    $("#navLife").hover(function () {
+        $("#theDes").html("My Life");
+    },
+        function () {
+            resetDes();
+        });
+
+    $("#Main").css("display", "block");
+
+    $(".navTab").click(function () {
+        $(".toShow").css("display", "none");
+        var aClick = $(this).attr('id');
+        var show = aClick.substring(3);
+        $("#" + show).css("display", "block");
+    });
+    $(window).scroll(function (event) {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop) {
+            $(".transform-transition").addClass("transform-active");
+            console.log("down");
+        } else {
+            $(".transform-transition").removeClass("transform-active");
+            console.log("up");
+        }
+        lastScrollTop = st;
+    });
+});
+function resetDes() {
+    $("#theDes").html("");
+}
